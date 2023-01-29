f61d36454e51d63edd4d4b1eca7ad286e6ce7f8a
Marcos Minond <minond.marcos@gmail.com>
2023-01-29 15:26:13 -0700

    Inlining js

--- app/components/page.rkt
+++ app/components/page.rkt
@@ -45,8 +45,34 @@
   (:div 'class: (format "flash ~a" kind)
         (:div 'class: "flash-text" text)))
 
-(define js
-  (port->string (open-input-file "client/index.js")))
+(define js "
+const socket = new WebSocket('ws://localhost:8082')
+
+socket.addEventListener('open', (event) =>
+  setInterval(() =>
+    socket.send('ping'), 60000))
+
+socket.addEventListener('message', (event) => {
+  if (event.data === 'pong') {
+    return
+  }
+
+  const msg = JSON.parse(event.data)
+
+  switch (msg.notification) {
+    case 'feed-added':
+      fetch('/articles', { headers: { Accept: 'application/json' } })
+        .then((res) =>
+          res.json())
+        .then((res) =>
+          document.querySelector('main').innerHTML = res.html)
+      break
+
+    default:
+      console.log(msg)
+      break
+  }
+})")
 
 (define font-styles-url
   "https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap")
